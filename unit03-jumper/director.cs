7c42883a2d76523516f7b70a2b1102e399d47489
LazyBones1313 <91147485+LazyBones1313@users.noreply.github.com>
2022-02-04 17:39:14 -0700

    did a thing

---
+++ unit03-jumper/director.cs
@@ -0,0 +1,11 @@
+public class Director
+{
+    string userChoice = "";
+    Hangman hangman = new Hangman();
+    Console.WriteLine("   -----");
+    Console.WriteLine("   |    |");
+    Console.WriteLine("   |     ");
+    Console.WriteLine("   |     ");
+    Console.WriteLine("   |     ");
+    Console.WriteLine("_________     ");
+}

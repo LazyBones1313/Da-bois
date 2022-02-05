9949753d8ff116ce5790578ec68eb433c2320b88
Andrew Stamm <stamm.andrew.r@gamil.com>
2022-02-04 17:56:19 -0700

    did a thing

--- unit03-jumper/director.cs
+++ unit03-jumper/director.cs
@@ -1,6 +1,18 @@
+using System
+
 public class Director
 {
     string userChoice = "";
+
+    public Director() 
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        Console.WriteLine("This is a game");
+    }
     Hangman hangman = new Hangman();
     Console.WriteLine("   -----");
     Console.WriteLine("   |    |");

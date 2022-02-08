e59e0c7bb9672693af342534608da6dcd5b01169
Andrew Stamm <stamm.andrew.r@gamil.com>
2022-02-08 16:53:26 -0700

    worked on secretword class

--- unit03-jumper/Game/SecretWord.cs
+++ unit03-jumper/Game/SecretWord.cs
@@ -5,41 +5,52 @@ namespace unit03_jumper.Game
 {
     public class SecretWord
     {
-<<<<<<< HEAD
-        public int currentWord = 0;
-        public int currentWord2 = 0;
+        private string _fileName;
+
+        private List<string> _wordList = new List<string>();
+        private List<string> _guessedLetters = new List<string>();
+        private string _currentWord = "";
+        public int _currentWord2 = 0;
 
         public SecretWord()
         {
-            
+            _fileName = "Game/words.txt";
+            UploadListFromTxt(_fileName);
+            Pick();
         }
 
-        public int Pick()
+        private void UploadListFromTxt(string file)
+        {
+
+            string[] lines = System.IO.File.ReadAllLines(file);
+            foreach (string line in lines)
+            {
+                _wordList.Add(line);
+            }
+        }
+        public void Pick()
         {
             Random random = new Random();
-            int aWord = random.Next(1,10);
-            return aWord;
+            int aWordIndex = random.Next(_wordList.Count);
+            _currentWord = _wordList[aWordIndex];
         }
 
-        // public int convert()
-        // {
-        //     if (currentWord == 1)
-        //     {
-                
-        //     }
-        //     else
-        //     {
-        //         return "h";
-        //     }
-        // }
+        public void GetDisplay()
+        {
+            // Print secret word
+            //    or
+            // Return secret word
+        }
 
-    }   
-}
+        public void IsFound()
+        {
 
-=======
-        Random random = new Random();
-        int aWord = random.Next(1,10);
-        return aWord;
+        }
+        
+        public void ContainsLetter()
+        {
+            
+        }
     }
 }
->>>>>>> 56f674ca786fe09b510d7bfb8e8ced1fc9a34e7d
+
